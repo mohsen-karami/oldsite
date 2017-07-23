bce59b3b78c05cc25a7fe3b89c1b3f004663f5d5
mrgeo22 <mr.geo22@gmail.com>
2017-07-24 02:56:49 +0430

    Add publication section in persian language

--- per/Publications/publications.js
+++ per/Publications/publications.js
@@ -1,12 +1,12 @@
 /* Set Time and Date in the navbar */
-var dayName = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-var monthName = new Array('January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+var dayName = new Array('یکشنبه', 'دوشنبه', 'سه‌شنبه', 'چهارشنبه', 'پنج‌شنبه', 'جمعه', 'شنبه');
+var monthName = new Array('ژانویه', 'فوریه', 'مارس', 'آوریل', 'مه', 'ژوئن', 'ژوئیه', 'اوت', 'سپتامبر', 'اکتبر', 'نوامبر', 'دسامبر');
 var hourap = new Array(12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
 function showTime(){
     var dateObj = new Date();
     var dayDate = dateObj.getDate(), day = dateObj.getDay(), month = dateObj.getMonth(), year = dateObj.getFullYear(), hour = dateObj.getHours(), minutes = (dateObj.getMinutes()<=9?'0'+dateObj.getMinutes():dateObj.getMinutes());
-    var todayDate  = dayName[day]+' '+dayDate+' '+monthName[month]+', '+year;
-    var todayTime = hourap[hour]+':'+minutes+' '+(hour<=11?'AM':'PM');
+    var todayDate  = dayName[day]+' '+dayDate+' '+monthName[month]+' '+year;
+    var todayTime = hourap[hour]+':'+minutes+' '+(hour<=11?'پیش از ظهر':'پس از ظهر');
     var dateDiv = document.getElementById('today');
     /*Set Current Date */
     if(dateDiv !== null) {
